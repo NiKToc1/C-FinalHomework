12e19c0ebd5749ff13e9e4b8eae3af43413b1b68
NiKToc <niktoc20@gmail.com>
2023-07-18 02:39:35 +0500

    task itself

---
+++ task/Program.cs
@@ -0,0 +1,27 @@
+﻿void Sorting(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if(firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] firstArray = new string[5] {"Hello", "2", "world", ":-)", "-2"};
+string[] secondArray = new string[firstArray.Length];
+
+Sorting(firstArray, secondArray);
+PrintArray(secondArray);
